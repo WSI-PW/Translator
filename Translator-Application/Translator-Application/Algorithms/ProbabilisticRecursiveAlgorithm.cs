3ebd04146a50e50bdbe62051bf8d2a1dca902743
xsnailu <xsnailu@gmail.com>
2021-05-07 15:36:51 +0200

    Algorithm bug fix

--- Translator-Application/Translator-Application/Algorithms/ProbabilisticRecursiveAlgorithm.cs
+++ Translator-Application/Translator-Application/Algorithms/ProbabilisticRecursiveAlgorithm.cs
@@ -10,9 +10,56 @@ namespace Translator_Application.Algorithms
     class ProbabilisticRecursiveAlgorithm<T> : IAlgorithm<T>
         where T : IDatabase
     {
+        private string GetArrayElementsFromTo(string[] array, int from, int to)
+        {
+            StringBuilder senteceToTranslate = new StringBuilder();
+            for (int index = from; index < to; index++)
+            {
+                senteceToTranslate.Append(array[index]);
+                senteceToTranslate.Append(" ");
+            }
+
+            // remove last " "
+            if(senteceToTranslate.Length != 0)
+            {
+                senteceToTranslate.Remove(senteceToTranslate.Length - 1, 1);
+            }
+
+            return senteceToTranslate.ToString();
+        }
+
         public string Solve(T database, string text)
         {
-            throw new NotImplementedException();
+            if (text.Length == 0)
+            {
+                return "";
+            }
+
+            //split sentence into words
+            string[] words = text.Split(' ');
+
+            for (int n = words.Length; n >= 1; n--)
+            {
+                for (int m = 0; m + n <= words.Length; m++)
+                {
+                    // podciąg zdania długości n zaczynający się od indeksu i;
+                    string subSentence = this.GetArrayElementsFromTo(words, m, m + n);
+
+                    string translatedSubSentence = database.Translate(subSentence);
+
+                    if (translatedSubSentence != null)
+                    {
+                        string leftSubString = this.GetArrayElementsFromTo(words, 0, m - 1).ToString();
+                        string rightSubString = this.GetArrayElementsFromTo(words, m + n, words.Length).ToString();
+
+                        return this.Solve(database, leftSubString) + " "
+                            + translatedSubSentence + " "
+                                + this.Solve(database, rightSubString);
+                    }
+                }
+            }
+
+            return "###";
         }
     }
 }
